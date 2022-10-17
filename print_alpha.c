88d99eaef900bd91eb1b3db33324e8c6614f2258
LENNON NGADI PAUL <111058059+LendoHub@users.noreply.github.com>
2022-10-17 12:12:43 +0100

    Update print_alpha.c

--- print_alpha.c
+++ print_alpha.c
@@ -8,7 +8,7 @@
  * if a flag is passed to _printf
  * Return: number of char printed
  */
-int print_string(va_list l, flags_t *f)
+ print_string(va_list l, flags_t *f)
 {
     char *s = va_arg(l, char *);
 
